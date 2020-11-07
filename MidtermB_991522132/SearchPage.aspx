ddd9e6661b2d32bae797096856aca4ceda1be6cc
Suzi Ryu <smsarah911@gmail.com>
2020-11-07 06:04:11 -0500

    Worked on SearchPage

--- MidtermB_991522132/SearchPage.aspx
+++ MidtermB_991522132/SearchPage.aspx
@@ -8,8 +8,16 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <asp:Label ID="Label2" runat="server" Text="Search Page " Font-Bold="True" Font-Size="Larger"></asp:Label><br /><br />
+        <asp:Label ID="Label1" runat="server" Text="Search by Distance " Font-Bold="True"></asp:Label>
+        <asp:Label ID="DisplayLabel" runat="server" Text="Distance = " ForeColor="#0066FF"></asp:Label><br /><br />
+        <asp:TextBox ID="txtInputDistance" runat="server"></asp:TextBox><br /><br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:SqlDataSource runat="server"></asp:SqlDataSource>
+        <br />
+        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        <asp:Button ID="btnBacktoHome" runat="server" Text="Back" OnClick="btnBacktoHome_Click"/>
     </form>
 </body>
 </html>
--- MidtermB_991522132/SearchPage.aspx.cs
+++ MidtermB_991522132/SearchPage.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,44 @@ namespace MidtermB_991522132
 {
     public partial class SearchPage : System.Web.UI.Page
     {
+        SqlConnection conn;
+
+        Double searchFloat;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        void Connectodb()
+        {
+            string constring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hami9\source\repos\MidtermB_991522132\MidtermB_991522132\App_Data\ABCTourDB_991522132.mdf;Integrated Security=True";
+            conn = new SqlConnection(constring);
+            conn.Open();
+        }
+
+        void SearchRecord()
+        {
+            Connectodb();
+            string query = "SELECT * FROM Destination WHERE Distance = Convert(float, '" + txtInputDistance.Text + "')";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            conn.Close();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchFloat = float.Parse(txtInputDistance.Text);
+            SearchRecord();
+            String searchDistance = txtInputDistance.Text;
+            DisplayLabel.Text = "Searching by Distance : " + searchDistance;
+        }
+
+        protected void btnBacktoHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("HomePage.aspx");
         }
     }
 }
