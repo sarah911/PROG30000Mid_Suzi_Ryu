65b35127173d91de494dc2f01f12117c4f735920
Suzi Ryu <smsarah911@gmail.com>
2020-11-07 06:25:01 -0500

    DisplayInfoPage done End of the Commit

--- MidtermB_991522132/DisplayInfoPage.aspx
+++ MidtermB_991522132/DisplayInfoPage.aspx
@@ -9,7 +9,13 @@
 <body>
     <form id="form1" runat="server">
         <div>
-        </div>
+            <asp:Label ID="Label1" runat="server" Text="Display Page" Font-Bold="True" Font-Size="Larger"></asp:Label><br />
+        </div><br />
+        Currently displaying distance 450 or more, and the visit cost no more than 300
+        <br /><br /><asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:SqlDataSource runat="server"></asp:SqlDataSource><br />
+        <asp:Button ID="btnBack" runat="server" Text="Back" OnClick="btnBack_Click" style="height: 26px" />
     </form>
 </body>
 </html>
--- MidtermB_991522132/DisplayInfoPage.aspx.cs
+++ MidtermB_991522132/DisplayInfoPage.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,35 @@ namespace MidtermB_991522132
 {
     public partial class DisplayInfoPage : System.Web.UI.Page
     {
+        SqlConnection conn;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            RenderResult();
+        }
+        void Connectodb()
+        {
+            string constring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hami9\source\repos\Practice_midterm\Practice_midterm\App_Data\ABCTourDB_991522132.mdf;Integrated Security=True";
+            conn = new SqlConnection(constring);
+            conn.Open();
+        }
 
+        void RenderResult()
+        {
+            //
+            Connectodb();
+            string query = "SELECT d.Name, v.Guest, v.Cost FROM Visit v INNER JOIN Destination d ON d.Name = v.Destination WHERE v.cost <= 300 AND d.Distance >= 450";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            conn.Close();
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("HomePage.aspx");
         }
     }
 }
